0f0d343861b04caf326a21e34e5e1fa7b0aea18b
dark <dark@DESKTOP-F0STRUP>
2020-12-20 21:30:10 +0200

    Добавьте файлы проекта.

---
+++ ConsoleAppEFDatabaseFirst/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace ConsoleAppEFDatabaseFirst
+{
+    public partial class User
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
